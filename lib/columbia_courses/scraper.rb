d33fc626d6098c5e88dce684f9c1020040549b54
Arvind Srinivasan <arvind.srinivasan@gmail.com>
2013-04-09 19:45:42 -0400

    get list of section pages with various parameters

--- lib/columbia_courses/scraper.rb
+++ lib/columbia_courses/scraper.rb
@@ -1,6 +1,7 @@
 require 'open-uri'
 require 'nokogiri'
 require 'pry'
+require 'json'
 
 class ColumbiaCourses::Scraper
   def initialize(params)
@@ -16,9 +17,18 @@ class ColumbiaCourses::Scraper
   end
 
   def append_url(url, params = {})
-    uri = url
+    uri = URI.parse(url)
     uri.query = uri.query.nil? ? params_to_query(params) : [uri.query, params_to_query(params)].join('&') unless params.empty?
     uri.to_s
+  end
+
+  def courses_from_search(page)
+    if @semesters
+      in_semester = lambda {|c| @semesters.any? {|s| c.include? s}}
+      return page.css(".l").map {|x| x.attr('href') if in_semester[x]}.compact.to_set
+    else
+      return page.css(".l").map {|x| x.attr('href')}.compact.to_set
+    end
   end
 
   def crawl_search_page(query)
@@ -37,24 +47,63 @@ class ColumbiaCourses::Scraper
       :proxystylesheet => "DoC",
       :proxyreload => 1
     }
-    first_page = append_url("http://search.columbia.edu/search/", params)
-    puts first_page
-    first_page = Nokogiri::HTML(open(append_url("http://search.columbia.edu/search/", params)))
-    binding.pry
-    return nil
+    first_page = Nokogiri::HTML(open(append_url("http://search.columbia.edu/search", params)))
+    num_results = first_page.css("#top_results_range_of_total").text().match(/100 of (\d+).*/)[1].to_i
+    course_pages = courses_from_search(first_page)
+    (100..num_results).step(100).each { |start| 
+      params[:start] = start
+      page = Nokogiri::HTML(open(append_url("http://search.columbia.edu/search", params)))
+      course_pages += courses_from_search(page)
+    }
+    return course_pages
   end
 
-  def crawl()
-    if @kw
-      return crawl_search_page(@kw.join(" "))
+  def crawl_dept_subj(subj = true, all_depts=false)
+    if @semesters
+      joined_sems = @semesters.map {|x| x.gsub(/\s/, '')}
+      in_semester = lambda {|c| joined_sems.any? {|s| c.include? s}}
     else
-      if @subjects.count
-        pages = @subjects.map { |x| "#{@base_url}sel/subj-#{x[0].capitalize}.html" }
+      in_semester = lambda { |c| true }
+    end
+    if subj
+      q = 'subj'
+      filter = @subjects
+    else
+      q = 'dept'
+      filter = @depts
+    end
+    if all_depts
+      filter = ('A'..'Z').to_a.map {|x| "#{@base_url}sel/dept-#{x}.html" }
+    end
+    pages = filter.map {|x| "#{@base_url}sel/#{q}-#{x[0].capitalize}.html"}
+    sub_pages = Set.new
+    pages.each {|p|
+      h = Nokogiri::HTML(open(p)) 
+      sub_pages += h.css('tr').map {|row| row.css('td a').map{ |sem| "http://columbia.edu" + sem.attr('href') if in_semester[sem.text()]}.compact if filter.include? row.children[0].text()}.flatten.compact
+    }
+    course_pages = Set.new
+    sub_pages.each {|p|
+      page = Nokogiri::HTML(open(p))
+      links = page.css('tr td a').map { |x| "http://columbia.edu" + x.attr('href') if x.attr('href').include? "subj" and not x.attr('href').include? '_text.html'}.compact.to_set
+      course_pages += links
+    }
+    return course_pages
+  end
+
+  def crawl(outfile="temp.json")
+    if @kw
+      course_pages = crawl_search_page(@kw.join(" "))
+    else
+      if @subjects
+        course_pages = crawl_dept_subj(true)
       elsif @depts
-        pages = @depts.map { |x| "#{@base_url}sel/dept-#{x[0].capitalize}.html" }
+        course_pages = crawl_dept_subj(false)
       else
-        pages = ('A'..'Z').to_a.map {|x| "#{@base_url}sel/dept-#{x}.html" }
+        course_pages = crawl_all(false, true)
       end
     end
+    File.open(outfile, 'w') { |file|
+      file.write(JSON.dump(course_pages.to_a))
+    }
   end
 end
